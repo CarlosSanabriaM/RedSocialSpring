286b20f459a7d7aed77bbf69eab103ac3cefacf0
CarlosSanabriaM <uo250707@uniovi.es>
2018-02-13 22:09:44 +0100

    Primer commit

---
+++ sdi1-uo250707uo251893/src/main/java/com/uniovi/Sdi1Uo250707uo251893Application.java
@@ -0,0 +1,12 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1Uo250707uo251893Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1Uo250707uo251893Application.class, args);
+	}
+}
